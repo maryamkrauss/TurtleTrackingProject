5d05aee9617a6bd08aeea83c3355cb812f9d4482
maryamkrauss <mjk91@duke.edu>
2025-09-28 10:40:15 -0400

    Initial commit of ARGOSTrackingTool script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Maryam Krauss (mjk91@duke.edu)
+# Date:   Fall 2025
